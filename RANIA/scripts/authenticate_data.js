b5941aec6c0213a5eea298e475666d082f37e377
EmilyFrancis1031 <60359470+EmilyFrancis1031@users.noreply.github.com>
2023-03-29 12:03:38 -0400

    Updated authenticate-data.js
    Filled in authenticate data function. Errorcodes need added to message json

--- RANIA/scripts/authenticate_data.js
+++ RANIA/scripts/authenticate_data.js
@@ -2,18 +2,40 @@
 //Called by 01: receive_data
 //update this function as you see fit
 //THIS NEEDS TO CHANGE FOR CONNECTING THE DEVICE
+import TinyDB from "tinydb"
 function authenticate_data(data_packet) {
+    
     var errorcode = 100
+
+    acl = new TinyDB('./test.db')
     //query ACL
-    //results = query access list with auth_token
 
-    //if the auth_code exists in the ACL
-    //if results equals success then:
-    //set errorcode to success
-    //else:
-    //set errorcode
-    //end
+    acl.onReady = function(){
+        //query access list with auth_token
+        acl.getInfo(data_packet['meta_data'], function(err, key, value) {
+            //if error retreiving data
+            if (err) {
+              console.log(err);
+              return;
+            }
+            //if the auth_code is correct in the ACL
+            else if(value == data_packet['auth_token']){
+                //set errorcode to success
+                errorcode = 101;
+                
+            }
+            else{
+                //set errorcode
+                errorcode = 102;
+                //end 
+            }
+                
+            
+          });
+    }
+    
 
+    
     return errorcode  //return to 01
 
 }
