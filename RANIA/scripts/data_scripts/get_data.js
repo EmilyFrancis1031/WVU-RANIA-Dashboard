cffd5788148c5f5fec68752cc257300d88d418bd
EmilyFrancis1031 <60359470+EmilyFrancis1031@users.noreply.github.com>
2023-04-06 11:39:41 -0400

    New messages and get_data

--- RANIA/scripts/data_scripts/get_data.js
+++ RANIA/scripts/data_scripts/get_data.js
@@ -1,1 +1,31 @@
 //calls tiny_db get
+import TinyDB from "tinydb"
+export function get_data(data_packet) {
+    
+    var errorcode = 301
+
+    db_path = data_packet['sender']+'/_data.json'
+    db = new TinyDB('./'+db_path)
+    //query ACL
+
+    db.onReady = function(){
+        //query access list with auth_token
+        db.getInfo(data_packet['meta_data'], function(err, key, value) {
+            //if error retreiving data
+            if (err) {
+              //console.log(err);
+              errorcode = 301;
+              return errorcode;
+            }
+            else{
+                //set errorcode
+                newkey = key
+                return {newkey: value}
+                //end 
+            }
+          });
+    }
+    
+    return errorcode  //return to 01
+
+}
