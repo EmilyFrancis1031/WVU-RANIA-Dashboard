08054973b97947d88184d511e61b705c55eefdeb
EmilyFrancis1031 <60359470+EmilyFrancis1031@users.noreply.github.com>
2023-04-18 11:58:36 -0400

    Update destroy_database.js

--- RANIA/scripts/data_scripts/destroy_database.js
+++ RANIA/scripts/data_scripts/destroy_database.js
@@ -3,14 +3,32 @@ var fs = require("fs");
 const dotenv = require("dotenv").config();
 
 async function destroy_database(data_packet) {
-  const to_remove = data_packet["data"]["to_remove"];
-  db_path = process.env.DB_DEVICE_ROOT_PATH + to_remove;
-  try {
-    fs.unlinkSync(db_path);
+  db_path =
+    process.env.DB_DEVICE_ROOT_PATH +
+    data_packet["data"]["device_name"] +
+    "/" +
+    data_packet["data"]["db_name"] +
+    ".json";
 
-    console.log("Delete File successfully.");
-  } catch (error) {
-    console.log(error);
+  var result = null;
+
+  const Result = await new Promise((resolve, reject) => {
+    let result = undefined;
+    try {
+      fs.unlinkSync(db_path);
+      result = 1;
+      console.log("Deleted File successfully.");
+      resolve(result);
+    } catch (error) {
+      result = 0;
+      reject(error);
+    }
+  });
+
+  if (Result == null) {
+    return result;
+  } else {
+    return result;
   }
 }
 
