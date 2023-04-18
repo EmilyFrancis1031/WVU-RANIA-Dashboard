9513c3bf382d75d1f2d32a512e00f4fcbf4ca61c
EmilyFrancis1031 <60359470+EmilyFrancis1031@users.noreply.github.com>
2023-04-18 12:33:44 -0400

    Update remove_data.js

--- RANIA/scripts/data_scripts/remove_data.js
+++ RANIA/scripts/data_scripts/remove_data.js
@@ -1,7 +1,48 @@
 //calls tiny_db remove
 var TinyDB = require("tinydb");
 async function remove_data(data_packet) {
-  return "Remove Data";
+  var errorcode = 310;
+
+  db_path =
+    process.env.DB_DEVICE_ROOT_PATH +
+    data_packet["data"]["device_name"] +
+    "/" +
+    data_packet["data"]["db_name"] +
+    "_data.json";
+
+  db = new TinyDB(db_path);
+
+  const dbResult = await new Promise((resolve) => {
+    let result = undefined;
+    db.onReady = function () {
+      db.findByIdAndRemove(
+        data_packet["data"]["k"],
+        function (err, key, value) {
+          if (err) {
+            console.error(err);
+            errorcode = 310;
+          }
+
+          result = { data: value };
+          resolve(result);
+        }
+      );
+    };
+  });
+
+  // const resultPromise = new Promise(db.onReady)
+
+  // resultPromise
+  // .then(console.log("[promise_accepted]",result))
+  // .catch(console.log("[promise_rejected]: ",result))
+
+  console.log("[get_data]: ", dbResult);
+  //return to 01
+  if (dbResult == null) {
+    return errorcode;
+  } else {
+    return dbResult;
+  }
 }
 
 module.exports = remove_data;
