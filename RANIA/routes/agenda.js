8d63f374dcb97822f4dfa829b7094c1f69f1c793
cragstein <cragstein@gmail.com>
2023-04-19 14:03:22 -0400

    navigatable pages

--- RANIA/routes/agenda.js
+++ RANIA/routes/agenda.js
@@ -1,80 +1,16 @@
 var express = require('express');
 var router = express.Router();
-var bodyParser = require('body-parser');
-var TinyDB = require('tinydb');
+
 var path = require("path");
-var ap = path.join("Agenda", "agenda");
-var insert_data = require("../scripts/data_scripts/insert_data");
-
-
-// Initialize TinyDB
-var db = new TinyDB('./db.json');
-
-// Set up body-parser
-router.use(bodyParser.urlencoded({ extended: true }));
-router.use(bodyParser.json());
+var ap = path.join("Agenda", "agenda")
 
 /* GET agenda page. */
-router.get('/', async function(req, res, next) {
-  const events = await db.gethAll('calendar_events') || [];
-  res.render(ap, { title: 'RANIA - Resident Agenda', events });
+router.get('/', function(req, res, next) {
+  res.render(ap, { title: 'RANIA - Resident Agenda' });
 });
 
 router.get('/modify', (req, res) => {
   res.send('Ability to modify agenda');
-});
-
-router.post('/add-event', async function(req, res, next) {
-  const title = req.body.eventTitle;
-  const date = req.body.eventDate;
-  const time = req.body.eventTime;
-  const event = { title, date, time };
-
-  const result = await insert_data({ data: { k: title, v: event }, db: 'calendar_events' });
-  if (result === 301) {
-    res.status(500).send('Failed to insert data');
-  } else {
-    res.redirect('/agenda');
-  }
-});
-
-router.post('/remove-event', async function(req, res, next) {
-  const title = req.body.eventTitle;
-  const date = req.body.eventDate;
-  const time = req.body.eventTime;
-  const event = { title, date, time };
-
-  const dbResult = await new Promise((resolve) => {
-    db.onReady = function () {
-      db.getInfo(title, function (err, value) {
-        if (err) {
-          console.log(err);
-          return;
-        }
-        if (value == null) {
-          console.log(`[getInfo] No value found for ${title}`);
-          resolve(null);
-        } else {
-          console.log(`[getInfo] ${title}: ${value}`);
-          db.removeInfo(title, function (err) {
-            if (err) {
-              console.log(err);
-              resolve(null);
-            } else {
-              console.log(`[removeInfo] ${title}`);
-              resolve(value);
-            }
-          });
-        }
-      });
-    };
-  });
-
-  if (dbResult == null) {
-    res.status(500).send('Failed to remove data');
-  } else {
-    res.redirect('/agenda');
-  }
 });
 
 module.exports = router;
--- RANIA/routes/dashboard.js
+++ RANIA/routes/dashboard.js
@@ -9,5 +9,19 @@ var dbpWizard = path.join("Wizards", "devices", "devices")
 router.get('/', function (req, res, next) {
   res.render(dbp, { title: 'RANIA - Dashboard' });
 });
+const devices = db.get('devices').value();
+
+router.get('/', function(req, res) {
+  // Render the dashboard view with the devices variable
+  res.render('dashboard', { devices });
+});
+
+router.get('/device/:id', function(req, res) {
+  const deviceId = req.params.id;
+  // Query the database for information about the specified device
+  const deviceInfo = db.get(`devices[${deviceId}]`).value();
+  // Render the device status view with the retrieved information
+  res.render('device_status', { deviceInfo });
+});
 
 module.exports = router;
