895347b05c50894edac71b08ce7f4e07726f4bfd
EmilyFrancis1031 <60359470+EmilyFrancis1031@users.noreply.github.com>
2023-05-17 09:11:08 -0400

    Update remove_data.js

--- RANIA/scripts/data_scripts/remove_data.js
+++ RANIA/scripts/data_scripts/remove_data.js
@@ -10,7 +10,7 @@ async function remove_data(data_packet) {
     data_packet["data"]["db_name"] +
     ".json";
 
-  db = new TinyDB(db_path);
+  /*db = new TinyDB(db_path);
 
   const dbResult = await new Promise((resolve) => {
     let result = undefined;
@@ -28,7 +28,7 @@ async function remove_data(data_packet) {
         }
       );
     };
-  });
+  });*/
 
   // const resultPromise = new Promise(db.onReady)
 
@@ -36,13 +36,29 @@ async function remove_data(data_packet) {
   // .then(console.log("[promise_accepted]",result))
   // .catch(console.log("[promise_rejected]: ",result))
 
-  console.log("[get_data]: ", dbResult);
+  //console.log("[get_data]: ", dbResult);
   //return to 01
-  if (dbResult == null) {
+  /*if (dbResult == null) {
     return errorcode;
   } else {
     return dbResult;
+  }*/
+  const jsonData = JSON.parse(fs.readFileSync(db_path, 'utf8'));
+
+  // Check if the searchKey exists in the JSON data
+  if (jsonData.hasOwnProperty(searchKey)) {
+    // Delete the key from the JSON data
+    delete jsonData[data_packet["data"]["k"]];
+
+    // Write the updated JSON back to the file
+    fs.writeFileSync(db_path, JSON.stringify(jsonData, null, 2));
+
+    console.log(`Key "${searchKey}" deleted successfully from ${filePath}.`);
+  } else {
+    console.log(`Key "${searchKey}" not found in ${filePath}. No changes made.`);
   }
+
+
 }
 
 module.exports = remove_data;
